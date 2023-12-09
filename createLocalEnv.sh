6817c004210b2f7a5b83f0f1b62b40db4f0dfd0b
pmgzo <pmgzo@pmgzo>
2023-12-09 22:00:58 +0100

    feat(env): setup envs with docker compose: persistant volume, use different port for env test

--- createLocalEnv.sh
+++ createLocalEnv.sh
@@ -1,14 +1,14 @@
 # This is a simple script to setup databases for dev and test environment, 
 # at the end the user should be able to run test and start the local server
 
-# db for local dev
-docker run --name postgres-db -e POSTGRES_PASSWORD=dumbypassword -p 5432:5432 -d postgres
-# db for tests
-docker run --name postgres-db-test -e POSTGRES_PASSWORD=dumbypassword -p 6000:5432 -d postgres
-
 # create these files for targeting the corresponding database
-echo "DATABASE_URL=\"postgresql://postgres:dumbypassword@localhost:5432/postgres\"" > .env.dev
-echo "DATABASE_URL=\"postgresql://postgres:dumbypassword@localhost:6000/postgres\"" > .env.test
+echo "DATABASE_URL=\"postgresql://postgres:dumbypassword@localhost:5432/postgres\"\nREDIS_PORT=6379" > .env.dev
+echo "DATABASE_URL=\"postgresql://postgres:dumbypassword@localhost:6000/postgres\"\nREDIS_PORT=7002\nCOMPOSE_FILE=compose-test.yml" > .env.test
+
+# db for local dev
+docker compose up -d
+# db for tests
+docker compose --env-file ".env.test" -p chat-app-backend-test up -d
 
 # push prisma schema to databases
 npx dotenv -e .env.dev -- npx prisma db push
