d6ccda82f5b06ca6f49216b124813168f532bad6
pmgzo <pierremickael.gonzalo@gmail.com>
2024-01-17 18:58:15 +0100

    fix(env): fix error

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	app.enableCors({ origin: process.env.NODE_ENV === 'production' ? 'http://localhost:8080': `http://${process.env.FRONTEND_URL}` });
+	app.enableCors({ origin: process.env.NODE_ENV === 'production' ? `http://${process.env.FRONTEND_URL}` : 'http://localhost:8080' });
 	await app.listen(3000);
 	console.log(`Application is running on: ${await app.getUrl()}`);
 }
