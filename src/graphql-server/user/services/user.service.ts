808e31a7381fd4ff59a6bb181a1d3eb35117e489
pmgzo <pierremickael.gonzalo@gmail.com>
2024-06-12 15:15:09 +0200

    feat(auth): change pass encryption by argonId2

--- src/graphql-server/user/services/user.service.ts
+++ src/graphql-server/user/services/user.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from '../../../prisma/prisma.service';
-import { getHash, generateSalt } from '../utils/hash';
 import { UserExistArguments } from '../models/user.model';
+import { hash, verify } from 'argon2';
+
+const SALT_LENGTH = 16;
+const randomCharList =
+	'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&\'()*+,./:;<=>?@\\[\\] ^_`{|}~-';
 
 @Injectable()
 export class UserService {
@@ -27,13 +31,13 @@ export class UserService {
 	}
 
 	async createUser(name: string, password: string): Promise<User> {
-		const salt: string = generateSalt();
+		const salt: string = this.generateSalt();
 
 		return await this.prismaService.user.create({
 			data: {
 				name: name,
 				salt: salt,
-				password: getHash({ password: password, salt }),
+				password: await hash(password, { salt: Buffer.from(salt, 'utf8') }),
 			},
 		});
 	}
@@ -41,14 +45,28 @@ export class UserService {
 	async verifyCredentials(name: string, password: string): Promise<User> {
 		const user: User = await this.findUser(name);
 
-		const createdPassword: string = getHash({
-			password: password,
-			salt: user.salt,
+		const digest: string = await hash(password, {
+			salt: Buffer.from(user.salt, 'utf8'),
 		});
 
-		if (createdPassword !== user.password) {
+		try {
+			await verify(digest, password);
+		} catch {
 			throw new Error('Wrong user credentials');
 		}
+
 		return user;
+	}
+
+	private generateSalt(): string {
+		let result = '';
+
+		for (let i = 0; i < SALT_LENGTH; i++) {
+			result += randomCharList.charAt(
+				Math.floor(Math.random() * randomCharList.length),
+			);
+		}
+
+		return result;
 	}
 }
